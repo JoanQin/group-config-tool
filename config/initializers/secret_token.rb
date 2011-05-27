9eaf5711538b46ba348af0d0c75f9b8d669a88fd
JoanQin <qin.42@osu.edu>
2011-05-27 15:21:37 -0400

    first commit group category created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroupConfig::Application.config.secret_token = '5e9d656fe52a3f92a301e4d421306e3eb8e86457170c8c66295a4d8009f7d6d85b68e0bf0fdd36d9d18c7e37c4c75fb9acc88291ab620fb6d4f766b0239a061d'
